061dc1756bb0137c0d7d541e9166531495cce868
souf139 <soufianboushaba12@gmail.com>
2023-11-04 11:04:12 +0100

    fix tailwind in my project

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./src/**/*.vue', './public/index.html'],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
   },
-};
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p class="text-sky-500 dark:text-sky-400">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
